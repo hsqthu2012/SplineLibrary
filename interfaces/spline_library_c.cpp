e8ec3c5b0f2098be86a0e298f181cb2697be609e
hesuqin <hesuqin@meituan.com>
2023-09-25 17:44:02 +0800

    feat: add C interface prototype

---
+++ interfaces/spline_library_c.cpp
@@ -0,0 +1,12 @@
+#include "spline_library_c.h"
+#include "spline.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ interfaces/spline_library_c.h
@@ -0,0 +1,14 @@
+#ifndef SPLINE_LIBRARY_C_H
+#define SPLINE_LIBRARY_C_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SPLINE_LIBRARY_C_H
