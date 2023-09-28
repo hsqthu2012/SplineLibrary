2077a9b5e1c303f74f8c95e62510d26fb588efd1
hesuqin <hesuqin@meituan.com>
2023-09-28 16:02:50 +0800

    feat: test 21 points Gauss-Legendre quadrature

--- spline_library/utils/calculus.h
+++ spline_library/utils/calculus.h
@@ -19,6 +19,8 @@ public:
         //it's not easy to compute the points/weights just given the number of points.
         //it involves computing every root of a polynomial. which can obviously be done, but not in a reasonable amount of code
         // ref to https://pomax.github.io/bezierinfo/legendre-gauss.html if more NUM_POINTS are needed
+
+        // NUM_POINTS = 13
         std::array<floating_t, NUM_POINTS> quadraturePoints = {
             floating_t( 0.0000000000000000),
             floating_t(-0.2304583159551348),
@@ -50,6 +52,56 @@ public:
             floating_t(0.0404840047653159),
             floating_t(0.0404840047653159)
         };
+
+        // NUM_POINTS = 21
+        // const size_t NUM_POINTS = 21;
+        // std::array<floating_t, NUM_POINTS> quadraturePoints = {
+        //     floating_t( 0.0000000000000000),
+        //     floating_t(-0.1455618541608951),
+        //     floating_t( 0.1455618541608951),
+        //     floating_t(-0.2880213168024011),
+        //     floating_t( 0.2880213168024011),
+        //     floating_t(-0.4243421202074388),
+        //     floating_t( 0.4243421202074388),
+        //     floating_t(-0.5516188358872198),
+        //     floating_t( 0.5516188358872198),
+        //     floating_t(-0.6671388041974123),
+        //     floating_t( 0.6671388041974123),
+        //     floating_t(-0.7684399634756779),
+        //     floating_t( 0.7684399634756779),
+        //     floating_t(-0.8533633645833173),
+        //     floating_t( 0.8533633645833173),
+        //     floating_t(-0.9200993341504008),
+        //     floating_t( 0.9200993341504008),
+        //     floating_t(-0.9672268385663063),
+        //     floating_t( 0.9672268385663063),
+        //     floating_t(-0.9937521706203895),
+        //     floating_t( 0.9937521706203895)
+        // };
+
+        // std::array<floating_t, NUM_POINTS> quadratureWeights = {
+        //     floating_t(0.1460811336496904),
+        //     floating_t(0.1445244039899700),
+        //     floating_t(0.1445244039899700),
+        //     floating_t(0.1398873947910731),
+        //     floating_t(0.1398873947910731),
+        //     floating_t(0.1322689386333375),
+        //     floating_t(0.1322689386333375),
+        //     floating_t(0.1218314160537285),
+        //     floating_t(0.1218314160537285),
+        //     floating_t(0.1087972991671484),
+        //     floating_t(0.1087972991671484),
+        //     floating_t(0.0934444234560339),
+        //     floating_t(0.0934444234560339),
+        //     floating_t(0.0761001136283793),
+        //     floating_t(0.0761001136283793),
+        //     floating_t(0.0571344254268572),
+        //     floating_t(0.0571344254268572),
+        //     floating_t(0.0369537897708525),
+        //     floating_t(0.0369537897708525),
+        //     floating_t(0.0160172282577743),
+        //     floating_t(0.0160172282577743)
+        // };
 
         floating_t halfDiff = (b - a) / 2;
         floating_t halfSum = (a + b) / 2;
